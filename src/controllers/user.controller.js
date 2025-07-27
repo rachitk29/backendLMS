aba32396a84259b122fc36bf22cf984069d8e0ac
rachitk29 <rachitkumar2953@gmail.com>
2025-07-27 14:41:46 +0530

    all register controllers bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/ApiError.js"
+import {ApiError} from "../utils/apiError.js"
 import { User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -184,4 +184,8 @@ const logoutUser = asyncHandler(async(req, res) => {
 })
 
 
-export { registerUser, loginUser, logoutUser };
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+};
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-// middleware/auth.middleware.js
+/* // middleware/auth.middleware.js
 import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
@@ -21,6 +21,43 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
 
     if (!user) {
         throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+});
+ */
+
+
+// middleware/auth.middleware.js
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    const token =
+        req.cookies?.accessToken ||
+        req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+        throw new ApiError(401, "Unauthorized request: Token missing");
+    }
+
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    } catch (error) {
+        // 👇 yahi change important hai
+        throw new ApiError(401, "Invalid or malformed token");
+    }
+
+    const user = await User.findById(decodedToken?._id).select(
+        "-password -refreshToken"
+    );
+
+    if (!user) {
+        throw new ApiError(401, "Access token is valid but user not found");
     }
 
     req.user = user;
